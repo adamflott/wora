0b8528b90bf9383164e834e2f05be7258e3f7872
Adam Flott <adam@adamflott.com>
2024-01-11 21:00:22 -0500

    Add libstatgrab for nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@ let
   pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
 in pkgs.clangStdenv.mkDerivation {
   name = "wora";
-  buildInputs = [ pkgs.cargo pkgs.rustc pkgs.libclang pkgs.pkg-config pkgs.clippy ];
+  buildInputs = [ pkgs.libstatgrab pkgs.cargo pkgs.rustc pkgs.libclang pkgs.pkg-config pkgs.clippy ];
   LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
 }
