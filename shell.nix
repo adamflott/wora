79cac3decd64a71cb9118946996f05bd4251d942
Adam Flott <adam@adamflott.com>
2026-05-23 15:53:10 -0400

    No longer using nixshell

--- shell.nix
+++
@@ -1,7 +0,0 @@
-let
-  pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
-in pkgs.clangStdenv.mkDerivation {
-  name = "wora";
-  buildInputs = [ pkgs.cargo pkgs.rustc pkgs.libclang pkgs.pkg-config pkgs.clippy ];
-  LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
-}
