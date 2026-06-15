deb360b2d2dd497055ef112c4743a2936cefc9f4
Adam Flott <adam@adamflott.com>
2026-06-14 21:07:06 -0400

    actually report the inner error

--- src/errors.rs
+++ src/errors.rs
@@ -10,31 +10,31 @@ use thiserror::Error;
 pub enum WoraSetupError {
     #[error("dirs")]
     Dirs,
-    #[error("io")]
+    #[error("io: {0}")]
     IO(#[from] std::io::Error),
     #[error("ioerrno")]
     IOErrno(Errno),
-    #[error("env vars")]
+    #[error("env vars: {0}")]
     ParseEnvVar(#[from] std::env::VarError),
-    #[error("addr parse")]
+    #[error("addr parse: {0}")]
     ParseIP(#[from] std::net::AddrParseError),
-    #[error("str")]
+    #[error("str: {0}")]
     Str(String),
-    #[error("notify")]
+    #[error("notify: {0}")]
     FSNotify(#[from] notify::Error),
-    #[error("dir missing")]
+    #[error("dir missing: ({0:?})")]
     DirectoryDoesNotExistOnFilesystem(std::path::PathBuf),
-    #[error("boot marker")]
+    #[error("boot marker: {0}")]
     BootMarker(#[from] BootMarkerError),
-    #[error("metric {0}")]
+    #[error("metric: {0}")]
     O11y(#[from] O11yError),
 }
 
 #[derive(Clone, Error, Debug)]
 pub enum NewWorkloadError {
-    #[error("args")]
+    #[error("args: {0}")]
     Args(String),
-    #[error("env")]
+    #[error("env: {0}")]
     Env(String),
 }
 
@@ -46,9 +46,9 @@ impl From<std::env::VarError> for NewWorkloadError {
 
 #[derive(Debug, Error)]
 pub enum VfsError {
-    #[error("io")]
+    #[error("io: {0}")]
     Io(#[from] std::io::Error),
-    #[error("notify")]
+    #[error("notify: {0}")]
     Notify(#[from] notify::Error),
     #[error("project directories unavailable for app {0}")]
     ProjectDirsUnavailable(String),
@@ -57,23 +57,23 @@ pub enum VfsError {
 #[derive(Debug, Error)]
 #[error(transparent)]
 pub enum SetupFailure {
-    #[error("setup: I/O")]
+    #[error("setup: I/O: {0}")]
     IO(#[from] std::io::Error),
-    #[error("setup: Errno")]
+    #[error("setup: Errno: {0}")]
     Errno(#[from] Errno),
     #[error("setup: logger")]
     Logger,
-    #[error("setup: parse int")]
+    #[error("setup: parse int: {0}")]
     ParseInt(#[from] std::num::ParseIntError),
-    #[error("setup: unknown system user")]
+    #[error("setup: unknown system user: {0}")]
     UnknownSystemUser(String),
     #[cfg(target_os = "linux")]
-    #[error("setup: capability")]
+    #[error("setup: capability: {0}")]
     #[cfg(target_os = "linux")]
     Capability(#[from] CapsError),
-    #[error("JSON parsing error")]
+    #[error("JSON parsing error: {0}")]
     JSON(#[from] serde_json::Error),
-    #[error("setup: vfs")]
+    #[error("setup: vfs: {0}")]
     Vfs(#[from] VfsError),
     #[error("setup: path is not valid UTF-8: {0:?}")]
     NonUtf8Path(std::path::PathBuf),
@@ -82,35 +82,35 @@ pub enum SetupFailure {
 #[derive(Debug, Error)]
 #[error(transparent)]
 pub enum MainEarlyReturn {
-    #[error("io")]
+    #[error("io: {0}")]
     IO(#[from] std::io::Error),
-    #[error("vfs")]
+    #[error("vfs: {0}")]
     Vfs(#[from] VfsError),
-    #[error("notify")]
+    #[error("notify: {0}")]
     Notify(#[from] notify::Error),
-    #[error("wora setup")]
+    #[error("wora setup: {0}")]
     WoraSetup(#[from] WoraSetupError),
-    #[error("workload")]
+    #[error("workload: {0}")]
     NewWorkload(#[from] NewWorkloadError),
-    #[error("setup")]
+    #[error("setup: {0}")]
     SetupFailed(#[from] SetupFailure),
     #[error("already running: {0}")]
     AlreadyRunning(std::path::PathBuf),
-    #[error("exit code")]
+    #[error("exit code: {0}")]
     UseExitCode(i8),
-    #[error("clap")]
+    #[error("clap: {0}")]
     ArgParsingClap(#[from] clap::error::Error),
-    #[error("env vars")]
+    #[error("env vars: {0}")]
     EnvParser(#[from] EnvVarsParseError),
-    #[error("logger")]
+    #[error("logger: {0}")]
     SetupError(#[from] log::SetLoggerError),
 }
 
 #[derive(Debug, Error)]
 pub enum LoggerSetupError {
-    #[error("logger: io")]
+    #[error("logger: io: {0}")]
     IO(#[from] std::io::Error),
-    #[error("logger: setup")]
+    #[error("logger: setup: {0}")]
     SetupError(#[from] log::SetLoggerError),
 }
 
@@ -125,9 +125,9 @@ pub enum EnvVarsParseError {}
 
 #[derive(Debug, Error)]
 pub enum ReloadError {
-    #[error("reload: vfs")]
+    #[error("reload: vfs: {0}")]
     Vfs(#[from] VfsError),
-    #[error("reload: notify")]
+    #[error("reload: notify: {0}")]
     Notify(#[from] notify::Error),
     #[error("reload: {0}")]
     Message(String),
